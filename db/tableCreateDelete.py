88e6ba92bd3343efc486f6a283e2fe614d5f2baa
Owner <Owner@DESKTOP-CE403MJ>
2019-06-16 10:38:06 -0500

    Couple of tests missing recent updates now work

--- db/tableCreateDelete.py
+++ db/tableCreateDelete.py
@@ -10,7 +10,7 @@ or updates.
 '''
 if (platform.system() == "Windows"):
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://localhost:8000")
-    ddbClient = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
+    ddbClient = boto3.client('dynamodb', region_name='us-east-1', endpoint_url='http://localhost:8000')
 else:
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     ddbClient = boto3.client('dynamodb')
--- test/cards/datalayerTest.py
+++ test/cards/datalayerTest.py
@@ -51,101 +51,107 @@ class TestDataLayer(unittest.TestCase):
         datalayer.deletePlayer(player)  
          
     def test_Table(self):
-        table = datalayer.getOrCreateSavedTable('notATableId')
-        print(table)
-         
-        tableII = datalayer.getOrCreateSavedTable(table.tableId)
-        print(tableII)        
-         
-        self.assertEqual(table.tableId, tableII.tableId)
-         
-        tableII.addPlayers(buildPlayers(5))
-        tableII.setDealerAtRandom()
-        tableII.setBlinds()
-        tableII.prepareForNextHand()
-        tableII.currentBet = 500
-         
-        datalayer.updateTable(tableII)
-        tableIII = datalayer.getOrCreateSavedTable(tableII.tableId)
-         
-        self.assertEqual(tableII.tableId, tableIII.tableId)
-        self.assertEqual(tableII.blind, tableIII.blind)
-        self.assertEqual(tableII.currentBet, tableIII.currentBet)
-        self.assertEqual(tableII.players[0].name, tableIII.players[0].name)
-        self.assertEqual(tableII.players[1].name, tableIII.players[1].name)
-        self.assertEqual(tableII.players[2].name, tableIII.players[2].name)
-        self.assertEqual(tableII.players[3].name, tableIII.players[3].name)
-        self.assertEqual(tableII.players[4].name, tableIII.players[4].name)
-        self.assertEqual(tableII.statusId, tableIII.statusId)
-                 
-        datalayer.deleteTable(tableII)
+        try:
+            table = datalayer.getOrCreateSavedTable('notATableId')
+            print(table)
+             
+            tableII = datalayer.getOrCreateSavedTable(table.tableId)
+            print(tableII)        
+             
+            self.assertEqual(table.tableId, tableII.tableId)
+             
+            tableII.addPlayers(buildPlayers(5))
+            tableII.setDealerAtRandom()
+            tableII.setBlinds()
+            tableII.prepareForNextHand()
+            tableII.currentBet = 500
+             
+            datalayer.updateTable(tableII)
+            tableIII = datalayer.getOrCreateSavedTable(tableII.tableId)
+             
+            self.assertEqual(tableII.tableId, tableIII.tableId)
+            self.assertEqual(tableII.blind, tableIII.blind)
+            self.assertEqual(tableII.currentBet, tableIII.currentBet)
+            self.assertEqual(tableII.players[0].name, tableIII.players[0].name)
+            self.assertEqual(tableII.players[1].name, tableIII.players[1].name)
+            self.assertEqual(tableII.players[2].name, tableIII.players[2].name)
+            self.assertEqual(tableII.players[3].name, tableIII.players[3].name)
+            self.assertEqual(tableII.players[4].name, tableIII.players[4].name)
+            self.assertEqual(tableII.statusId, tableIII.statusId)
+        finally:        
+            datalayer.deleteTable(table)
+            datalayer.deleteTable(tableII)
+            datalayer.deleteTable(tableIII)
+                        
          
     def testFindATableForPlayer(self):
-        players = buildPlayers(8)
-        table = datalayer.findATableForPlayer(players[0])
-        firstTableId = table.tableId
-         
-        for index in range(1,7):
-            table = datalayer.findATableForPlayer(players[index])
-            self.assertTrue(players[index] in table.players)
-            self.assertEquals(table.tableId, firstTableId)
- 
-        table2 = datalayer.findATableForPlayer(players[7])
-        self.assertNotEquals(table2.tableId, firstTableId)
-         
-        datalayer.deleteTable(table)
-        datalayer.deleteTable(table2)
+        try:
+            players = buildPlayers(7)
+            table = datalayer.findATableForPlayer(players[0])
+            firstTableId = table.tableId
+             
+            for index in range(1,6):
+                table = datalayer.findATableForPlayer(players[index])
+                self.assertTrue(players[index] in table.players)
+                self.assertEquals(table.tableId, firstTableId)
+     
+            table2 = datalayer.findATableForPlayer(players[6])
+            self.assertNotEquals(table2.tableId, firstTableId)
+        finally:    
+            datalayer.deleteTable(table)
+            datalayer.deleteTable(table2)
         
     def testResetTables(self):
-        players = buildPlayers(1)
-        table = datalayer.findATableForPlayer(players[0])
-        firstTableId = table.tableId
-        table.blind = 5
-        table.currentBet = 1000
-        datalayer.updateTable(table)
-        
-        table2 = datalayer.getOrCreateSavedTable(firstTableId)
-        self.assertEquals(table2.tableId, firstTableId)
-        self.assertEqual(1, len(table.players))
-        datalayer.resetUnusedTables(4)
-        
-        logging.log(logging.INFO, "Waiting 5 seconds to see if player is automatically evicted")
-        time.sleep(5)
-        table3 = datalayer.getOrCreateSavedTable(firstTableId)
-        self.assertEqual(1, len(table3.players))
-        self.assertEquals(table3.tableId, firstTableId)
-        
-        datalayer.updateTable(table)
-        datalayer.deleteTable(table)
+        try:
+            players = buildPlayers(1)
+            table = datalayer.findATableForPlayer(players[0])
+            firstTableId = table.tableId
+            table.blind = 5
+            table.currentBet = 1000
+            datalayer.updateTable(table)
+            
+            table2 = datalayer.getOrCreateSavedTable(firstTableId)
+            self.assertEquals(table2.tableId, firstTableId)
+            self.assertEqual(len(table2.players), len(table.players))
+            datalayer.resetUnusedTables(4)
+            
+            logging.log(logging.INFO, "Waiting 5 seconds to see if player is automatically evicted")
+            time.sleep(5)
+            table3 = datalayer.getOrCreateSavedTable(firstTableId)
+            self.assertEqual(1, len(table3.players))
+            self.assertEquals(table3.tableId, firstTableId)
+        finally:
+            datalayer.updateTable(table)
+            datalayer.deleteTable(table2)
         
     def test_cards_on_table_stay (self):
-        
-        players = buildPlayers(1)
-        
-        table = datalayer.findATableForPlayer(players[0])
-        table.dealToTable(3)
-        datalayer.updateTable(table)
-        
-        cardsInDeck = table.deck
-        originalTableId = table.tableId
-        
-        players = buildPlayers(4)
-        for player in players:
-            table = datalayer.findATableForPlayer(player)
-            self.assertEqual(len(cardsInDeck.cards), len(table.deck.cards), "Deck should be the same length") 
-            self.assertEqual(originalTableId, table.tableId, "And it should be the same table") 
-        
-        count = 5
-        self.assertEqual(count, len(table.players))
-        
-        
-        for player in table.players:
-            count = count -1
-            table.removePlayer(player)
+        try:
+            players = buildPlayers(1)
+            
+            table = datalayer.findATableForPlayer(players[0])
+            table.dealToTable(3)
+            datalayer.updateTable(table)
+            
+            cardsInDeck = table.deck
+            originalTableId = table.tableId
+            
+            players = buildPlayers(4)
+            for player in players:
+                table = datalayer.findATableForPlayer(player)
+                self.assertEqual(len(cardsInDeck.cards), len(table.deck.cards), "Deck should be the same length") 
+                self.assertEqual(originalTableId, table.tableId, "And it should be the same table") 
+            
+            count = 5
             self.assertEqual(count, len(table.players))
-            datalayer.deletePlayer(player)
-
-        datalayer.deleteTable(table)
+            
+            
+            for player in table.players:
+                count = count -1
+                table.removePlayer(player)
+                self.assertEqual(count, len(table.players))
+                datalayer.deletePlayer(player)
+        finally:
+            datalayer.deleteTable(table)
 
 def random_string(length):
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
